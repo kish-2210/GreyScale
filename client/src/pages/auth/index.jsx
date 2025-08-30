46940b59af03ed3c157f66ef6ddb4a1456f1f521
kish-2210 <kishansg32@gmail.com>
2025-08-30 22:57:15 +0530

    feat: auth page and server setup

---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import Background from "@/assets/Background.png"
+import Victory from "@/assets/victory.svg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button';
+const Auth = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [value, setValue] = useState("login");
+
+  const handleLogin = async () => { }
+  const handleSignup = async () => { }
+
+  return (
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-custom-gradient">
+      <div className="relative z-10 h-full w-full flex items-center justify-center">
+        <div className='h-[90vh] relative rounded-xl  bg-neutral-950  border border-neutral-800 shadow-lg overflow-hidden bg-[linear-gradient(to_right,rgba(51,51,51,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(51,51,51,0.2)_1px,transparent_1px)] bg-[size:20px_20px]  p-6 w-[75vw] md:w-[90vw] md:p-8 grid md:grid-cols-2'>
+          <div className="flex flex-col gap-10 items-center justify-center">
+            <div className="flex items-center justify-center flex-col">
+              <div className="flex items-center justify-center">
+                <h1 className="text-white text-5xl font-bold md:text-6xl">
+                  Vibrant
+                </h1>
+              </div>
+              <p className="font-medium text-center text-[#A1A1AA]">
+                Fill in the details to get started with best chat app
+              </p>
+            </div>
+
+            <div className="flex items-center justify-center w-full">
+              <Tabs  defaultValue="login" className="w-full md:w-[90%]">
+                <TabsList className="bg-transparent w-full flex gap-3">
+                  <TabsTrigger
+                    className={`w-full text-gray-400 hover:text-white rounded-xl py-4 text-lg data-[state=active]:bg-white data-[state=active]:text-black data-[state=active]:shadow-lg hover:bg-neutral-800 transition-colors duration-300`}
+                    value="login"
+                  >
+                    Login
+                  </TabsTrigger>
+                  <TabsTrigger
+                    className={`w-full text-gray-400 hover:text-white rounded-xl py-4 text-lg data-[state=active]:bg-white data-[state=active]:text-black data-[state=active]:shadow-lg hover:bg-neutral-800 transition-colors duration-300`}
+                    value="signup"
+                  >
+                    Sign up
+                  </TabsTrigger>
+                </TabsList>
+                <TabsContent value="login" className="flex flex-col gap-5 mt-10 justify-center items-center">
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    className="rounded-2xl p-6 text-white bg-greyI border-1 border-black focus:ring-0 focus:ring-offset-0 focus:ring-white focus:border-white"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    className="rounded-2xl p-6 text-white bg-greyI border-1 border-black focus:ring-0 focus:ring-offset-0 focus:ring-white focus:border-white"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    className="rounded-xl p-6 text-lg w-full font-bold text-black bg-white hover:bg-Bhover"
+                    onClick={handleLogin}
+                  >Login</Button>
+                </TabsContent >
+                <TabsContent value="signup" className="flex flex-col gap-5 mt-10">
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    className="rounded-2xl p-6 text-white bg-greyI border-1 border-black focus:ring-0 focus:ring-offset-0 focus:ring-white focus:border-white"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    className="rounded-2xl p-6 text-white bg-greyI border-1 border-black focus:ring-0 focus:ring-offset-0 focus:ring-white focus:border-white"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+
+                  <Input
+                    placeholder="Confirm password"
+                    type="password"
+                    className="rounded-2xl p-6 text-white bg-greyI border-1 border-black focus:ring-0 focus:ring-offset-0 focus:ring-white focus:border-white"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+
+                  <Button
+                    className="rounded-xl p-6 text-lg w-full font-bold text-black bg-white hover:bg-Bhover"
+                    onClick={handleSignup}
+                  >Sign up</Button>
+                </TabsContent>
+              </Tabs>
+            </div>
+          </div>
+          <div className="hidden md:flex justify-center items-center">
+            <img src={Background} alt="background" className='h-[550px]' />
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Auth
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+const MONGO_URI  = process.env.MONGO_URI;
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET","POST","PUT","PATCH","DELETE"],
+    credentials: true,
+}))
+
+app.use(cookieParser())
+app.use(express.json());
+
+
+const server = app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
+
+mongoose.connect(MONGO_URI)
+.then(()=>console.log('DB Connection Successful'))
+.catch(err=> console.log("ERROR!!!"))
